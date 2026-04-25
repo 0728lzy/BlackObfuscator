5c5f1f721fc11edbd3680c3fe55595f75677439d
Daniel Liu <1276223841@qq.com>
2026-04-26 06:40:38 +0800

    配置插件

---
+++ blackobfuscator-gradle-plugin/src/main/java/zym/top/blackobfuscator/gradle/VariantSigningInfo.java
@@ -0,0 +1,33 @@
+package zym.top.blackobfuscator.gradle;
+
+import java.io.File;
+
+class VariantSigningInfo {
+    private final File storeFile;
+    private final String storePassword;
+    private final String keyAlias;
+    private final String keyPassword;
+
+    VariantSigningInfo(File storeFile, String storePassword, String keyAlias, String keyPassword) {
+        this.storeFile = storeFile;
+        this.storePassword = storePassword;
+        this.keyAlias = keyAlias;
+        this.keyPassword = keyPassword;
+    }
+
+    File getStoreFile() {
+        return storeFile;
+    }
+
+    String getStorePassword() {
+        return storePassword;
+    }
+
+    String getKeyAlias() {
+        return keyAlias;
+    }
+
+    String getKeyPassword() {
+        return keyPassword;
+    }
+}
